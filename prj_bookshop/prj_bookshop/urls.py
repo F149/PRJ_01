82df34848449094fb5ac9601c2ab9da0cc3cb6f8
sm1rnov <smirnov.avc@gmail.com>
2019-03-25 20:12:31 +0200

    Added user registration

--- prj_bookshop/prj_bookshop/urls.py
+++ prj_bookshop/prj_bookshop/urls.py
@@ -15,11 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from shopapp import views
+
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
 
-]
+    path('shopapp/sign-in/', auth_views.LoginView.as_view(), name="shopapp-sign-in"),
+    path('shopapp/sign-out/', auth_views.LogoutView.as_view(), name="shopapp-sign-out"),
+
+    path('shopapp/', views.shopapp_home, name='shopapp-home'),
+
+    path('shopapp/sign-up/', views.shopapp_sign_up, name='shopapp-sign-up'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from shopapp.forms import UserForm, BookshopForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
 def home(request):
+    return redirect(shopapp_home)
+
+
+@login_required(login_url='/shopapp/sign-in/')
+def shopapp_home(request):
     return render(request, 'shopapp/home.html', {})
+
+
+def shopapp_sign_up(request):
+    user_form = UserForm()
+    book_shop_form = BookshopForm()
+
+    if request.method == 'POST':
+        user_form =  UserForm(request.POST)
+        book_shop_form = BookshopForm(request.POST, request.FILES)
+
+        if user_form.is_valid() and book_shop_form.is_valid():
+            new_user = User.objects.create_user(**user_form.cleaned_data)
+            new_book_shop = book_shop_form.save(commit=False)
+            new_book_shop.owner = new_user
+            new_book_shop.save()
+
+            login(request, authenticate(
+                username = user_form.cleaned_data['username'],
+                password = user_form.cleaned_data['password'],
+            ))
+
+            return redirect(shopapp_home)
+
+    return render(request, 'shopapp/sign_up.html', {
+        'user_form': user_form,
+        'book_shop_form': book_shop_form,
+    })
