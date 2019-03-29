7e3098d5a9e1c0d55152ac166c9f04643aa49fe9
sm1rnov <smirnov.avc@gmail.com>
2019-03-29 20:58:31 +0200

    Decor sidebar

--- prj_bookshop/prj_bookshop/urls.py
+++ prj_bookshop/prj_bookshop/urls.py
@@ -31,9 +31,6 @@ urlpatterns = [
     path('shopapp/', views.shopapp_home, name='shopapp-home'),
 
     path('shopapp/sign-up/', views.shopapp_sign_up, name='shopapp-sign-up'),
-    path ('shopapp/account/', views.shopapp_account, name='shopapp-account'),
-    path ('shopapp/book/', views.shopapp_book, name='shopapp-book'),
-
-
-
+    path('shopapp/account/', views.shopapp_account, name='shopapp-account'),
+    path('shopapp/book/', views.shopapp_book, name='shopapp-book'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shopapp/models.py
+++ shopapp/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class BookShop(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='pizzashop')
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='bookshop')
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    logo =models.ImageField(upload_to='pizzashop_logo/', blank=False)
+    logo = models.ImageField(upload_to='bookshop_logo/', blank=False)
 
     def __str__(self):
         return self.name
